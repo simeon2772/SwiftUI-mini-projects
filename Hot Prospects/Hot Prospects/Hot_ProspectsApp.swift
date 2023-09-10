8d12b1629b43fc19ed7112393fc29ed15dd2d354
simeon2772 <100004667+simeon2772@users.noreply.github.com>
2023-09-10 14:12:13 +0300

    Add files via upload

---
+++ Hot Prospects/Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Simeon Ivanov on 10.09.23.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
