72b7e19cd039fe474a7fbbb2b98bf59acedc52df
simeon2772 <100004667+simeon2772@users.noreply.github.com>
2023-09-14 17:19:46 +0300

    Add files via upload

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Simeon Ivanov on 6.07.23.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: "BGN"))
+                            .foregroundColor(item.amount <= 10 ? Color.red : Color.blue)
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Simeon Ivanov on 7.07.23.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
