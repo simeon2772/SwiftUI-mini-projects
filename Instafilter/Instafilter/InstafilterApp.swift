69eac2e70222e05419c69e53a24cdebbb77aff77
simeon2772 <100004667+simeon2772@users.noreply.github.com>
2023-10-13 18:17:08 +0300

    Add files via upload

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Simeon Ivanov on 8.09.23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
