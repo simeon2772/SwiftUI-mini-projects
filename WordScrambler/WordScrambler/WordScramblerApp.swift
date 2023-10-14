ebf07d3a92450075c8627707653a0e2f45f36ba9
simeon2772 <100004667+simeon2772@users.noreply.github.com>
2023-10-14 09:53:03 +0300

    Add files via upload

---
+++ WordScrambler/WordScrambler/WordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblerApp.swift
+//  WordScrambler
+//
+//  Created by Simeon Ivanov on 3.07.23.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
