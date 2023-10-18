f64f3544f27a3b1923c8428c7a55218456b9beeb
simeon2772 <100004667+simeon2772@users.noreply.github.com>
2023-10-18 20:23:13 +0300

    Add files via upload

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Simeon Ivanov on 3.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var order = Order()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnable.animation())
+                    
+                    if order.specialRequestEnable {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery Details")
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Simeon Ivanov on 3.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
